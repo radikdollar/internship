ded36d5ff3a18cab06db85e56716a64e06aaddfa
Радион Вахромеев <radikvahromeev@gmail.com>
2026-06-21 19:03:09 +0800

    задание 9 ардуино скетч

--- task9/sketch.ino
+++ task9/sketch.ino
@@ -1,17 +1,23 @@
-#define RED_PIN     9
-#define GREEN_PIN   10
-#define LDR_PIN     A0
+// Задание 9
+// Зелёный светодиод - двери открываются
+// Красный светодиод - двери закрываются
+// Фоторезистор имитирует приближение/удаление человека от дверей
 
-#define THRESHOLD   512
-#define CHECK_DELAY 1000
+#define RED_PIN     9    // красный светодиод (закрытая дверь)
+#define GREEN_PIN   10   // зелёный светодиод (открытая дверь)
+#define LDR_PIN     A0   // фоторезистор
 
-bool doorOpen = false;
+#define THRESHOLD   512  // порог срабатывания (человек "подошёл")
+#define CHECK_DELAY 1000 // через сколько мс повторно проверяем фоторезистор
+
+bool doorOpen = false; // по умолчанию дверь закрыта
 
 void setup() {
   pinMode(RED_PIN, OUTPUT);
   pinMode(GREEN_PIN, OUTPUT);
   Serial.begin(9600);
 
+  // По умолчанию горит светодиод закрытой двери
   closeDoor();
 }
 
@@ -19,16 +25,20 @@ void loop() {
   int lightValue = analogRead(LDR_PIN);
 
   if (lightValue > THRESHOLD) {
+    // Человек рядом - открываем дверь
     openDoor();
 
     delay(CHECK_DELAY);
 
+    // Проверяем ещё раз: человек всё ещё рядом?
     int recheckValue = analogRead(LDR_PIN);
     if (recheckValue > THRESHOLD) {
+      // Остаётся открытой, ничего не меняем
     } else {
       closeDoor();
     }
   } else {
+    // Человека рядом нет - дверь закрыта
     closeDoor();
   }
 }
